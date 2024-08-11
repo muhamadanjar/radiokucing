adcc9adff1ee6f08bef557b6f0840b3b85774179
Muhamad Anjar <arvanria@gmail.com>
2024-08-11 13:07:05 +0700

    feat: add marketplace

---
+++ salebymarketplace.py
@@ -0,0 +1,47 @@
+from load_env import *
+from utils.scrape_service import driver, time, By, EC, wait
+import gspread
+from auth import credentials
+from datetime import datetime
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("date")
+args = parser.parse_args()
+
+current_date = args.date
+
+date_now = datetime.strptime(current_date, "%Y-%m-%d")
+
+
+spreadsheet_id = get_env("SHEET_ID")
+client = gspread.authorize(credentials)
+workbook = client.open_by_key(spreadsheet_id)
+sheet = workbook.worksheet("DASHBOARD")
+
+driver.get(
+    "https://dashboard.olsera.co.id/reports/sales?path=salesdetailsmarketplace")
+
+
+
+wrapper = wait.until(EC.visibility_of_element_located(
+    (By.CSS_SELECTOR, '.content-wrapper')))
+
+summary = wait.until(EC.visibility_of_element_located(
+    (By.CSS_SELECTOR, '.summary')))
+table_contents = summary.find_elements(By.CSS_SELECTOR, 'table tbody tr')
+start_column = 3
+for content in table_contents:
+	td = content.find_elements(By.TAG_NAME, 'td')
+	for idx, tdv in enumerate(td):
+		if idx == 6:
+			val = int(tdv.get_attribute("innerHTML").replace(
+                    "IDR ", "").replace(".", ""))
+			print(val)
+			day = start_column + int(date_now.day)
+			print(f"H{day}")
+			sheet.update_acell(f"H{day}", val)
+		
+time.sleep(5)
+driver.quit()
--- salebysales.py
+++ salebysales.py
@@ -3,18 +3,7 @@ from utils.scrape_service import driver, time, By, EC, wait
 import gspread
 from auth import credentials
 
-
-driver.get("https://dashboard.olsera.co.id/reports/sales?path=salesbysalesman")
-wait.until(EC.visibility_of_element_located(
-    (By.CSS_SELECTOR, '.content-wrapper')))
-
-
-report_page = driver.find_element(By.CSS_SELECTOR, '.report-page')
-
-report_item = wait.until(EC.visibility_of_all_elements_located(
-    (By.CSS_SELECTOR, '.report-item')))
-
-print("report item", report_item)
+salesman = ['AMEL', 'IRSYAD', 'RIAN', 'WIDIA', 'WIWI']
 
 
 spreadsheet_id = get_env("SHEET_ID")
@@ -23,12 +12,59 @@ workbook = client.open_by_key(spreadsheet_id)
 
 sheet = workbook.worksheet("DASHBOARD")
 
+def update_cell(report_item):
+	report_item_button = report_item.find_element(By.CSS_SELECTOR, 'button')
 
-for item in report_item:
-	summary = item.find_element(By.CLASS_NAME, 'summary')
-	summary_val = summary.get_attribute('innerHTML')
-	sheet.update_acell(f"M9", summary_val)
+	report_item_button_text = (report_item_button.find_element(By.TAG_NAME, 'h4').get_attribute("innerHTML")).upper()
+
+	print(report_item_button_text)
+	if report_item_button_text in salesman:
+		report_item_button.click()
+
+		time.sleep(10)
+
+		summary = report_items = wait.until(EC.visibility_of_element_located(
+			(By.CSS_SELECTOR, '.summary')))
+
+		# table = summary.get_attribute("innerHTML")
+		# print("table",summary.get_attribute("innerHTML"))
+
+		contents = summary.find_elements(By.CSS_SELECTOR, 'table tbody tr')
+		for content in contents:
+			td = content.find_elements(By.TAG_NAME, 'td')
+			for idx, tdv in enumerate(td):
+				if idx == 2:
+					val = int(tdv.get_attribute("innerHTML").replace("IDR ", "").replace(".", ""))
+					print(val)
+					if report_item_button_text == "IRSYAD":
+						sheet.update_acell(f"N5", val)
+		time.sleep(2)
+		# driver.find_element(By.CSS_SELECTOR, 'button.el-button.el-icon-back').click()
+
+
+driver.get("https://dashboard.olsera.co.id/reports/sales?path=salesbysalesman")
+wait.until(EC.visibility_of_element_located(
+    (By.CSS_SELECTOR, '.content-wrapper')))
+
+
+report_page = driver.find_element(By.CSS_SELECTOR, '.report-page')
+
+
+report_items = wait.until(EC.visibility_of_all_elements_located(
+    (By.CSS_SELECTOR, '.report-item')))
+
+# print("report item", report_items)
+
+idx = 0
+total_report = len(report_items)
+
+for report_index in range(total_report):
+	report_item = report_items[report_index]
+	idx = report_index
+	update_cell(report_item)
+	
 
 time.sleep(5)
 
 driver.quit()
+
